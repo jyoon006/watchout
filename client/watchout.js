823a23bdf323d376e0b70e86c4ca69febcd6c20a
Jisoo Yoon <jyoon006@gmail.com>
2016-01-22 19:33:11 -0500

    successfully added data to enemies variable

--- client/watchout.js
+++ client/watchout.js
@@ -1,7 +1,29 @@
 // start slingin' some d3 here.
+var enemyPos = [[200,250], [100, 350], [400, 100], [175, 50], [300, 300]];
 
 var container = d3.select("body").append("svg").attr("height", "600px").attr("width", "600px")
                 .attr("class", "container");
 
 
-var enemy = container.append("circle").attr("cx", "10").attr("cy", "10"). attr("r", "20").attr("class", "enemy");
+
+var update = function(data){
+  // data join
+  var enemies = container.selectAll("circle").data(data);
+  console.log(enemies);
+  // // add new enemies
+  // enemies.enter().container.append("circle")
+  //   .attr("cx", function(){
+
+  //   })
+  //   .attr("cy", function(){
+
+  //   })
+  //   .attr("r", "20")
+  //   .attr("class", "enemy");
+  // // position callbacks
+  // var
+
+};
+
+// initial setup
+update(enemyPos);
